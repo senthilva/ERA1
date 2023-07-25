72ba204f7dd44d4cc192855a158664b556dff0db
senthilva <senthil.vinayagam@gmail.com>
2023-07-25 21:58:31 +0530

    Update utils.py

--- session11/utils.py
+++ session11/utils.py
@@ -16,25 +16,25 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 def transform_trainv2():
+    means = [0.4914, 0.4822, 0.4465]
+    stds = [0.2470, 0.2435, 0.2616]
     return A.Compose(
     [
-        A.CropAndPad(4,pad_mode=0, pad_cval=0, pad_cval_mask=0, 
-                     keep_size=False, sample_independently=True, interpolation=1, 
-                     always_apply=True),
-        A.RandomCrop(32,32,always_apply=True),
-        A.HorizontalFlip(p=0.5),
-        A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=0, always_apply=False, p=0.5),
-        #A.CoarseDropout(max_holes = 1, max_height=8, max_width=8, min_holes = 1, min_height=1, min_width=1, fill_value=0.5, mask_fill_value = None),
-        A.Normalize((0.4914, 0.4822, 0.4465), 
-                    (0.2023, 0.1994, 0.2010)),
+       
+        A.Normalize(mean=means, std=stds, always_apply=True),
+        A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
+        A.RandomCrop(height=32, width=32, always_apply=True),
+        A.HorizontalFlip(),
+        A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=means),
         ToTensorV2(),
     ])
       
 def transform_testv2():
+    means = [0.4914, 0.4822, 0.4465]
+    stds = [0.2470, 0.2435, 0.2616]
     return A.Compose(
     [
-        A.Normalize((0.4914, 0.4822, 0.4465), 
-                    (0.2023, 0.1994, 0.2010)),
+        A.Normalize(mean=means, std=stds, always_apply=True),
         ToTensorV2(),
     ])
 
