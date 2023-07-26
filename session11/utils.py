1649efb9370b046be502a4324c3051991fc49606
senthilva <senthil.vinayagam@gmail.com>
2023-07-26 21:23:51 +0530

    Update utils.py

--- session11/utils.py
+++ session11/utils.py
@@ -156,6 +156,7 @@ def denormalize(images, means, stds):
     stds = torch.tensor(stds).reshape(1, 3, 1, 1)
     return images * stds + means
     
+
 def display_misclassified_images(model,device,classes):
     print("\n********* Misclassified Images **************\n")
     model.eval()
@@ -163,7 +164,7 @@ def display_misclassified_images(model,device,classes):
     # Create a test loader with batch size equal to test data length
     test = torchvision.datasets.CIFAR10(
                                         root='./data', train=False, 
-                                        download=True, transform=transform_test())
+                                        download=True, transform=transform_testv2())
     
     # dataloader arguments - something you'll fetch these from cmdprmt
     dataloader_args = dict(shuffle=True, batch_size=len(test), 
@@ -184,42 +185,7 @@ def display_misclassified_images(model,device,classes):
             fig = plt.figure(figsize=(15, 20))
             for i, idx in enumerate(indexes[:10]):
                 ax = fig.add_subplot(2, 5, i+1)
-                denorm_images = denormalize(data.cpu(), [0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
-                ax.imshow(denorm_images[idx].squeeze().permute(1, 2, 0).clamp(0,1))
-                ax.set_title(f"Target = {classes[target[idx].item()]} \n Predicted = {classes[pred[idx].item()]}")
-
-            plt.show()
-
-
-def display_misclassified_images(model,device,classes):
-    print("\n********* Misclassified Images **************\n")
-    model.eval()
-
-    # Create a test loader with batch size equal to test data length
-    test = torchvision.datasets.CIFAR10(
-                                        root='./data', train=False, 
-                                        download=True, transform=transform_test())
-    
-    # dataloader arguments - something you'll fetch these from cmdprmt
-    dataloader_args = dict(shuffle=True, batch_size=len(test), 
-                           num_workers=2, pin_memory=True)
-    
-    # test dataloader
-    testloader = torch.utils.data.DataLoader(test, **dataloader_args)
-    
-    
-    with torch.no_grad():
-        for data, target in testloader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            # Get the indexes of images that are incorrectly classified
-            indexes = (pred.view(-1,) != target.view(-1,)).nonzero()
-            plt.ion()
-            fig = plt.figure(figsize=(15, 20))
-            for i, idx in enumerate(indexes[:10]):
-                ax = fig.add_subplot(2, 5, i+1)
-                denorm_images = denormalize(data.cpu(), [0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+                denorm_images = denormalize(data.cpu(), [0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616])
                 ax.imshow(denorm_images[idx].squeeze().permute(1, 2, 0).clamp(0,1))
                 ax.set_title(f"Target = {classes[target[idx].item()]} \n Predicted = {classes[pred[idx].item()]}")
 
