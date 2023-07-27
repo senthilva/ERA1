7425b78097b5d437b0e4568ad66813c5d156aa83
senthilva <senthil.vinayagam@gmail.com>
2023-07-27 22:12:21 +0530

    Update main.py

--- session11/main.py
+++ session11/main.py
@@ -171,7 +171,7 @@ for epoch in range(EPOCHS):
     # scheduler.step()
     test(net, device, test_loader)
 
-display_misclassified_images(net,device,list(classes))
+display_misclassified_images(net,device,list(classes),num_images=10)
 '''
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
